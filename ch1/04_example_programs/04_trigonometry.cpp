aadaae6d4234fb01a6c01fbf68e7470cfa463da4
Mustafa Zarkash <mustafataherzaky@outlook.com>
2022-11-30 13:01:07 +0200

    chapter 1 examples update

--- ch1/04_example_programs/04_trigonometry.cpp
+++ ch1/04_example_programs/04_trigonometry.cpp
@@ -12,18 +12,18 @@ using std::cin;
 using std::endl;
 
 int main(){
-	const float PI = 3.14 ; 
+    const float PI = 3.14 ; 
 
     float angle = 0 , angle_in_radians = 0 ;
 
     cout << "This program calculates (sin , cos , tan) of an angle:" << endl ;  // welcome message
 	
     cout << "Enter an Angle in degrees: " ; 
-	cin >> angle ; 
+    cin >> angle ; 
     
     angle_in_radians = angle * PI / 180 ; //convert degrees to radians 
 
-	cout << "sin(" <<  angle << ") = "<< sin(angle_in_radians) << endl ; 
+    cout << "sin(" <<  angle << ") = "<< sin(angle_in_radians) << endl ; 
     cout << "cos(" <<  angle << ") = "<< cos(angle_in_radians) << endl ; 
     cout << "tan(" <<  angle << ") = "<< tan(angle_in_radians) << endl ; 
 
